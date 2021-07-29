c6c3c20f72bc48be7e1ca41029b67e057f73861a
Joe Rivera <joe@lonestardata.io>
2021-07-29 11:28:54 -0500

    generate multiple charts

--- gen_csv.py
+++ gen_csv.py
@@ -1,4 +1,5 @@
 import csv
+import datetime
 import math
 import json
 import time
@@ -6,44 +7,76 @@ import uuid
 import zipfile
 
 
-OUTFILE_CSV = "instrument.csv"
+OUTFILE_CSVS = {
+    "inst0.csv": {
+        "start_time": 1.0,
+        "end_time": 11.0,
+        "num_samples": 100,
+        "amplitude": 0.5,
+        "freq": 4,
+        "phase": 0.0,
+        "y_offset": 0.0},
+    "inst1.csv": {
+        "start_time": 1.0,
+        "end_time": 11.0,
+        "num_samples": 100,
+        "amplitude": 0.5,
+        "freq": 4,
+        "phase": 0.0,
+        "y_offset": 0.0}}
+
+
 OUTFILE_JSON = "metadata.json"
 OUTFILE_ZIP = "Session_{}.zip".format(uuid.uuid1())
-SAMPLES = 100
-SAMPLE_TIME_INCR = 0.25
 
 
-with open(OUTFILE_CSV, 'w', newline='') as csvfile:
-    inst_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
-    inst_writer.writerow(['time', 'sample'])
-    sample_time = 0.0
-    for i in range(SAMPLES):
-        samp = i/(SAMPLES-1)
-        inst_writer.writerow([sample_time, math.sin(samp * 2*math.pi)])
-        sample_time = sample_time + SAMPLE_TIME_INCR
+def gen_chart(
+        filename, start_time=1.0, end_time=11.0, num_samples=100,
+        amplitude=0.5, freq=4, phase=0.0, y_offset=0):
+    """
+    Generate sinusoid.
+    """
+    with open(filename, 'w', newline='') as csvfile:
+        inst_writer = csv.writer(csvfile, quoting=csv.QUOTE_MINIMAL)
+        inst_writer.writerow(['time', 'sample'])
+        sample_time = start_time
+        inc = (end_time - start_time)/num_samples
+        while sample_time < end_time:
+            yt =  amplitude * math.sin(2*math.pi * freq * sample_time + phase) + y_offset
+            inst_writer.writerow([sample_time, yt])
+            sample_time += inc
+
+    return {
+        "chart_type": "line",
+        "title": "Test Data",
+        "x_label": "Time (s)",
+        "x_min": start_time,
+        "x_max": end_time,
+        "y_label": "Amplitude",
+        "y_min": -amplitude,
+        "y_max": amplitude}
+
 
 metadata = {
-    "timestamp": time.time(),
+    "vendor": "oval.bio",
+    "version": "1.0.0",
+    "source": "default",
+    "timestamp": str(datetime.datetime.now()),
     "first_name": "Test",
     "last_name": "Client",
     "email": "test@example.com",
     "gender": "Male",
     "birthdate": "1/1/1970",
     "weight": 180.0,
-    "chart_data": {
-        OUTFILE_CSV: {
-            "chart_type": "line",
-            "x_label": "Time (s)",
-            "x_min": 0.0,
-            "x_max": SAMPLES/SAMPLE_TIME_INCR,
-            "y_label": "Amplitude",
-            "y_min": -1.5,
-            "y_max": 1.5}}}
-
-with open(OUTFILE_JSON, "w") as json_file:
-    json.dump(metadata, json_file)
+    "chart_data": {}}
 
 
 with zipfile.ZipFile(OUTFILE_ZIP, mode="w") as session:
-    session.write(OUTFILE_CSV)
+    for csv_filename, kwargs in OUTFILE_CSVS.items():
+        meta = gen_chart(csv_filename, **kwargs)
+        metadata["chart_data"][csv_filename] = meta
+        session.write(csv_filename)
+
+    with open(OUTFILE_JSON, "w") as json_file:
+        json.dump(metadata, json_file)
     session.write(OUTFILE_JSON)
