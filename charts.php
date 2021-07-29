0d8d121fa70b220560fe5f896cc24e43060c857e
Joe Rivera <joe@lonestardata.io>
2021-07-28 22:37:58 -0500

    generating chart for each csv in the zip file

--- charts.php
+++ charts.php
@@ -2,14 +2,13 @@
 
 $media = get_attached_media('');
 foreach($media as $attachment) {
-  echo $attachment->guid;
-
 ?>
 
 <script type="text/javascript" src="/data/jquery-3.6.0.min.js"></script>
 <script type="text/javascript" src="/data/jszip/dist/jszip.min.js"></script>
 <script type="text/javascript" src="/data/jszip-utils/dist/jszip-utils.min.js"></script>
 <script src="/data/d3/d3.v4.js"></script>
+<script src="/data/d3-dsv/csv.js"></script>
 
 <div id="session_metadata"></div>
 <div id="instrument_charts"></div>
@@ -22,8 +21,150 @@ foreach($media as $attachment) {
     function showMetadata(elt, content) {
         elt.innerHTML +=
 	    "<p>" +
-	      JSON.stringify(content) +
+	      content.first_name + " " + content.last_name + "<br/>" +
+	      content.timestamp
 	    "</p>";
+    }
+
+    function showChart(name, data) {
+// set the dimensions and margins of the graph
+var margin = {top: 60, right: 30, bottom: 80, left: 60},
+    width = 560 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
+
+// append the svg object to the body of the page
+var svg = d3.select("#instrument_charts")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+
+  //Read the data
+  var csv_data = d3.csvParse(data);
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(csv_data, function(d) { return d.time; }))
+      .range([ 0, width ]);
+    xAxis = svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain([0, d3.max(csv_data, function(d) { return +d.sample; })])
+      .range([ height, 0 ]);
+    yAxis = svg.append("g")
+      .call(d3.axisLeft(y));
+
+      svg.append('text')
+      .attr('x', width/2)
+      .attr('y', -20)
+      .attr('text-anchor', 'middle')
+      .style('font-family', 'Helvetica')
+      .style('font-size', 20)
+      .text(name);
+
+      // X label
+      svg.append('text')
+      .attr('x', width/2)
+      .attr('y', height+40)
+      .attr('text-anchor', 'middle')
+      .style('font-family', 'Helvetica')
+      .style('font-size', 12)
+      .text('Time (MS)');
+      //
+      // Y label
+      svg.append('text')
+      .attr('text-anchor', "middle")
+      .attr('transform', 'translate(-40,' + height/2 + ')rotate(-90)')
+      .style('font-family', 'Helvetica')
+      .style('font-size', 12)
+      .text('Svpulse');
+
+    // Add a clipPath: everything out of this area won't be drawn.
+    var clip = svg.append("defs").append("svg:clipPath")
+        .attr("id", "clip")
+        .append("svg:rect")
+        .attr("width", width )
+        .attr("height", height )
+        .attr("x", 0)
+        .attr("y", 0);
+
+    // Add brushing
+    var brush = d3.brushX()                   // Add the brush feature using the d3.brush function
+        .extent( [ [0,0], [width,height] ] )  // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+        .on("end", updateChart)               // Each time the brush selection changes, trigger the 'updateChart' function
+
+    // Create the line variable: where both the line and the brush take place
+    var line = svg.append('g')
+      .attr("clip-path", "url(#clip)")
+
+    // Add the line
+    line.append("path")
+      .datum(csv_data)
+      .attr("class", "line")  // I add the class line to be able to modify this line later on.
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.time) })
+        .y(function(d) { return y(d.sample) })
+        )
+
+    // Add the brushing
+    line
+      .append("g")
+        .attr("class", "brush")
+        .call(brush);
+
+    // A function that set idleTimeOut to null
+    var idleTimeout
+    function idled() { idleTimeout = null; }
+
+    // A function that update the chart for given boundaries
+    function updateChart() {
+
+      // What are the selected boundaries?
+      extent = d3.event.selection
+
+      // If no selection, back to initial coordinate. Otherwise, update X axis domain
+      if(!extent){
+        if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
+        x.domain([ 4,8])
+      }else{
+        x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
+        line.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
+      }
+
+      // Update axis and line position
+      xAxis.transition().duration(1000).call(d3.axisBottom(x))
+      line
+          .select('.line')
+          .transition()
+          .duration(1000)
+          .attr("d", d3.line()
+            .x(function(d) { return x(d.time) })
+            .y(function(d) { return y(d.sample) })
+          )
+    }
+
+    // If user double click, reinitialize the chart
+    svg.on("dblclick",function(){
+      x.domain(d3.extent(csv_data, function(d) { return d.time; }))
+      xAxis.transition().call(d3.axisBottom(x))
+      line
+        .select('.line')
+        .transition()
+        .attr("d", d3.line()
+          .x(function(d) { return x(d.time) })
+          .y(function(d) { return y(d.sample) })
+      )
+      //location.reload();
+    });
+
     }
 
     var htmltext = JSZipUtils.getBinaryContent("<?=$attachment->guid;?>", function (err, data) {
@@ -37,10 +178,10 @@ foreach($media as $attachment) {
                 .then(function (zip) {
                     for(var name in zip.files) {
                         if (name.substring(name.lastIndexOf('.') + 1) === "csv") {
+			  var local_name = (' ' + name).slice(1);
                           zip.file(name).async("string").then(
 			    function(data) {
-			      //session_data[name] = data;
-			      // TODO: d3 chart below
+			      showChart(local_name, data);
 			    });
                         }
 			else if (name.substring(name.lastIndexOf('.') + 1) === "json") {
@@ -205,7 +346,7 @@ d3.csv("https://oval.bio/data/simplifies_extended.csv",
       //location.reload();
     });
 
-})
+});
 </script>
 <?
 }
